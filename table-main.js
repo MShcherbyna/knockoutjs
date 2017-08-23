edf1f028d62621456bc15382807aa88f7ca46ab1
boyandman <aleksandr.kryvtsun@boyman.io>
2017-08-23 19:08:23 +0300

    add checkbox

--- table-main.js
+++ table-main.js
@@ -1,10 +1,20 @@
 var ViewModel = function(first, last) {
+
     this.firstName = ko.observable('');
     this.lastName = ko.observable('');
     this.age = ko.observable();
+    this.sex = ko.observable();
+    this.isAdmin = ko.observable(false);
+    this.adminName = ko.observable("");
+    this.sexOptions = ['male','famale'];
+    
     this.fullName = ko.computed(function(){
     	return this.firstName() + " " + this.lastName();
     },this);
+    
+    this.notEmpty = ko.computed(function(){
+    	return !!this.firstName() || !!this.lastName() || !!this.sex() || !!this.age() || !!this.isAdmin();
+    },this);	
 };
  
 ko.applyBindings(new ViewModel()); 
